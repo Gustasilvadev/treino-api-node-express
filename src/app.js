0112748067e04abc70c3873b44ca2f4b96b95758
Gustasilvadev <gustamarsil@gmail.com>
2026-02-26 08:03:11 -0300

    Estrutura de autorização e autenticação

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+
+const path = require("path");
+require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+
 const logger = require("./middlewares/logger");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
 
+const authRoutes = require("./routes/auth.routes");
 const usersRoutes = require("./routes/users.routes");
 
 const app = express();
@@ -13,6 +19,7 @@ app.use(express.json());
 //Middlewares de log
 app.use(logger);
 
+app.use("/auth", authRoutes);
 app.use("/api/v1/users", usersRoutes);
 
 app.use(notFound);
